e95c7b34593fc7feb35def28f6b899eb2cdf9316
Rahi63 <36030289+Rahi63@users.noreply.github.com>
2022-08-02 17:19:02 +0200

    Add files via upload

---
+++ LockedMe01.java
@@ -0,0 +1,176 @@
+package mypackage;
+
+import java.io.*;
+import java.util.*;
+
+
+public class LockedMe01 {
+	
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		int choice = -1;
+		int subChoice = -1;
+		
+		System.out.println("Welcome to LockedMe.com");
+		System.out.println("Application Developer : Raheleh Samadi");
+		System.out.println("\nFile Menu Related Application");
+		
+		do {
+			
+			System.out.println("--------------------------------");
+			System.out.println("1: Display list of Files");
+			System.out.println("2: Display the user interface");
+			System.out.println("0: Exit");
+			
+			System.out.print("\nChoose your option: ");
+			
+			choice =scan.nextInt();
+			switch(choice)
+			{
+			case 1:
+				System.out.println("You are in Case 1");
+				System.out.println("-----------------");
+				LockedMe01.displayFile();		
+				break;
+			case 2:
+				System.out.println("You are in Case 2");
+				System.out.println("-----------------");
+				System.out.println("11: Add File");
+				System.out.println("12: Delete File");
+				System.out.println("13: Search File");
+				System.out.println("14: Back to Main Menu");
+				
+				System.out.print("Choose the operation to do...");
+				subChoice=scan.nextInt();
+				switch(subChoice)
+				{
+				case 11:
+					LockedMe01.addFile();
+					break;
+				case 12:
+					LockedMe01.deleteFile();
+					break;
+				case 13: 
+					LockedMe01.searchFile();
+					break;
+				case 0:
+					System.out.println("You are in Case 1");
+					break;
+				}
+				break;
+			case 0: 
+				System.out.println("You exit from application");
+				return;			
+			}
+			
+		}while(choice != 0);
+		
+
+	}
+		
+	public static void addFile() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter File name: ");
+		String name = sc.nextLine();
+		
+		try {
+			File fileObj = new File("D:\\Java_workspace\\Archive_Files", name);
+	        if (fileObj.createNewFile()) {
+	            System.out.println("File created: " + fileObj.getName());
+	        } else {
+	            System.out.println("File Exists Already");
+	        }
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+        
+        
+	}
+	
+	public static void deleteFile() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter file name to be created: ");
+		String filename = sc.nextLine();
+		try {
+			
+			File file= new File("D:\\Java_workspace\\Archive_Files", filename);  //file to be delete  
+			if(file.delete())   //returns Boolean value  
+				  
+					System.out.println(file.getName() + " was deleted");   //getting and printing the file name  
+			else  
+					System.out.printf("Failed to delete "+ filename);
+			
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	    
+	}
+	
+	public static void searchFile() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter File name to search: ");
+		String name = sc.nextLine();
+		try {	
+			File fileObj = new File("D:\\Java_workspace\\Archive_Files",name);
+		    if (fileObj.exists()) {
+		      System.out.println("File_Name: " + fileObj.getName());
+		      System.out.println("File_Readable " + fileObj.canRead());
+		      System.out.println("File_Writeable: " + fileObj.canWrite());
+		      System.out.println("File_Size in bytes " + fileObj.length());
+		    }
+		    else {
+		    System.out.println("File not exist");
+		   }
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	    
+	}
+	
+	public static void sortFile() {
+		
+		try {
+		File dir = new File("D:\\Java_workspace\\Archive_Files");
+
+	    File[] files = dir.listFiles();
+
+	    Arrays.sort(files, (f1, f2) -> f1.compareTo(f2));
+
+	      for (File file : files) {
+	         if (!file.isHidden()) {
+	            if (file.isDirectory()) {
+	               System.out.println("DIR \t" + file.getName());
+	            } else {
+	               System.out.println("FILE\t" + file.getName());
+	            }
+	         }
+	      }
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	   }
+	
+	public static void displayFile() {
+		try {
+		File folder = new File("D:\\Java_workspace\\Archive_Files");
+		
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) 
+			  System.out.println("File " + listOfFiles[i].getName());
+			else
+				System.out.println("There is No file");
+	
+		}
+		}catch(Exception ex)
+			{
+			System.out.println(ex.getMessage());
+			}
+	}
+}
